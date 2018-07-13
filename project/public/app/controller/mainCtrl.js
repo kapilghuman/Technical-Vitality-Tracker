953e8c0cce8502813559a7bf4256604f676d0e89
Prabhleen Kaur <32322112+prabhleennannar@users.noreply.github.com>
2018-07-13 16:11:51 +0530

    Create mainCtrl.js

---
+++ project/public/app/controller/mainCtrl.js
@@ -0,0 +1,33 @@
+angular.module('mainController', ['authServices'])
+
+.controller('mainCtrl', function(Auth, $timeout, $location){
+	var app = this;
+	
+	this.doLogIn = function(loginData){
+		app.loading = true;
+		app.errorMsg = false;
+		
+		Auth.login(app.loginData).then(function(data){
+			
+			if(data.data.success){
+				app.loading = false;
+				
+				//Create Success Message
+				app.successMsg = data.data.message;
+				
+				//Redirect to Register Page
+				$timeout(function(){
+					$location.path('/abc');
+				}, 2000);
+			}
+			
+			else{
+				app.loading = false;
+				//Create an Error Message
+				app.errorMsg = data.data.message;
+			}
+		});
+	};
+});
+
+
