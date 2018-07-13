e2b713a7a62d2246353b5a373cccbc4897314a5e
Prabhleen Kaur <32322112+prabhleennannar@users.noreply.github.com>
2018-07-13 16:10:22 +0530

    Update route.js

--- project/public/app/route.js
+++ project/public/app/route.js
@@ -1,10 +1,20 @@
-angular.module('appRoute', ['ngRoute'])
+angular.module('appRoutes', ['ngRoute'])
 
 .config(function($routeProvider, $locationProvider){
 	
 	$routeProvider.when('/', {
-		templateUrl: 'app/views/index.html'
-	})
+		templateUrl: 'app/views/pages/users/register.html',
+		controller: 'regCtrl',
+		controllerAs: 'register'
+	});
+	
+	$routeProvider.when('/login', {
+		templateUrl: 'app/views/pages/users/login.html'
+	});
+	
+	$routeProvider.when('/abc', {
+		templateUrl: 'app/views/pages/users/abc.html'
+	});
 	
 	$locationProvider.html5Mode({
 		enabled: true,
