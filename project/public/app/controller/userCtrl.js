9070e771821a02d0682f83ecc4a700473ace2945
Prabhleen Kaur <32322112+prabhleennannar@users.noreply.github.com>
2018-07-13 16:11:12 +0530

    Update userCtrl.js

--- project/public/app/controller/userCtrl.js
+++ project/public/app/controller/userCtrl.js
@@ -1,7 +1,33 @@
-angular.module('userController', [])
+var app = angular.module('userController', ['userServices']);
 
-.controller('logCtrl', function(){
-	var logUser = function(){
-		console.log('Hello');
+app.controller('regCtrl', function($http, $location, $timeout, User){
+	
+	var app = this;
+	
+	this.regUser = function(regData){
+		app.loading = true;
+		app.errorMsg = false;
+		
+		User.create(app.regData).then(function(data){
+			
+			if(data.data.success){
+				app.loading = false;
+				
+				//Create Success Message
+				app.successMsg = data.data.message;
+				
+				//Redirect to Register Page
+				$timeout(function(){
+					$location.path('/login');
+				}, 2000);
+			}
+			
+			else{
+				app.loading = false;
+				//Create an Error Message
+				app.errorMsg = data.data.message;
+			}
+		});
 	};
 });
+
