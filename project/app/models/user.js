f7abdd69ea7b14c8e8d3ce13f86df50f2e0c1c78
Prabhleen Kaur <32322112+prabhleennannar@users.noreply.github.com>
2018-07-13 16:38:06 +0530

    Create user.js

---
+++ project/app/models/user.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+var UserSchema = new Schema({
+	username: { type:String, required:true, lowercase:true, unique:true },
+	userId: { type:String, required:true, lowercase:true, unique:true },
+	password: { type:String, required:true },
+	role: { type:String, required:true }
+});
+
+
+//encrypting password
+UserSchema.pre('save', function(next){
+	var user = this;
+	bcrypt.hash(user.password, null, null, function(err, hash){
+		if(err) return next(err);
+		user.password = hash;
+		
+		 next();
+	});
+});
+
+
+UserSchema.methods.comparePassword = function(password){
+	return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
