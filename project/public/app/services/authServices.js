42c90729220d72c0b3014b95a416c100e740ad06
Prabhleen Kaur <32322112+prabhleennannar@users.noreply.github.com>
2018-07-13 16:35:40 +0530

    Create authServices.js

---
+++ project/public/app/services/authServices.js
@@ -0,0 +1,11 @@
+angular.module('authServices', [])
+
+.factory('Auth', function($http){
+	authFactory = {};
+	
+	authFactory.login = function(loginData){
+		return $http.post('/api/authenticate', loginData);
+	}
+	
+	return authFactory;
+});
