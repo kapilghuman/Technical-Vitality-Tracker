c428423bb54e384816071ce60b92483b749c117f
Kapil Ghuman <kapil.ghuman007@gmail.com>
2018-07-16 03:13:54 +0530

    Update app.js

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,2 +1,7 @@
-angular.module('userApp', ['appRoutes', 'userController', 'userServices', 'mainController', 'authServices']);
+angular.module('userApp', ['appRoutes', 'userController', 'userServices', 'mainController', 'authServices'])
+
+.config(function($httpProvider){
+    $httpProvider.interceptors.push('AuthInterceptors');
+});
+
 
