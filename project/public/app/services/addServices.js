d51b17d29e1e0dfba75578fabd1343717bdb3c44
Prabhleen Kaur <32322112+prabhleennannar@users.noreply.github.com>
2018-07-30 22:13:03 +0530

    Update addServices.js

--- project/public/app/services/addServices.js
+++ project/public/app/services/addServices.js
@@ -5,15 +5,23 @@ angular.module('addServices', [])
 	
 	addFactory.create = function(addData){
 		return $http.post('/api/adds', addData);
-	}
+	};
 	
 	addFactory.getAccomplishments = function() {
       		return $http.get('/api/accomplishment/');  
     	};
 	
+	addFactory.getStatus = function(id){
+		return $http.get('/api/edit/' +id);
+	};
+	
 	addFactory.deleteAccomplishment = function(title) {
-        	return $http.delete('/api/accomplishment/' + title);
-    	};
+               return $http.delete('/api/accomplishment/' + title);
+         };
+	
+	addFactory.editStatus = function(id){
+		return $http.put('/api/edit', id);
+	};
 	
 	addFactory.getPerformer = function(dates) {
 		return $http.post('/api/performer/',dates);  
